485d1828054304a20d50659307463bba6e084a17
CJCB77 <coral.johnny.2@gmail.com>
2022-01-14 18:56:53 -0500

    Added push animation to key when key is pressed

--- piano.js
+++ piano.js
@@ -13,9 +13,14 @@ const c4_sound = new Audio("./notes/c4.wav")
 
 document.addEventListener("keyup", (e) => {
     const audio = document.querySelector(`audio[data-key="${e.key}"]`)
+    const note = document.querySelector(`div[data-key="${e.key}"]`)
     if(!audio) return;
     audio.currentTime = 0
     audio.play()
+    note.classList.add("key-pressed")
+    setTimeout(() => {
+    note.classList.remove("key-pressed")
+    }, 250)
 
 })
 
